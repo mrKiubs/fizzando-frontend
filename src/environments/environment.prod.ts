ac410e85312f5d413b5428ecba5ffb6973e4c7db
mrKiubs <crescimbeni@gmail.com>
2025-10-24 16:18:49 +0200

    fix52

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://fizzando-backend-production.up.railway.app',
-  apiUrl: 'https://fizzando-backend-production.up.railway.app', // alias
-  siteUrl: 'https://fizzando-frontend-production.up.railway.app',
+  apiBaseUrl: 'https://fizzando-backend-production.up.railway.app', // niente :8080, Railway espone su 443
 };
